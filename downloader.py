3586b45ed2080f42f3421be42c0784fc82342800
Heinz <heinz.samuelsson@gmail.com>
2017-05-27 23:03:58 +0200

    downloader moved to own file

---
+++ downloader.py
@@ -0,0 +1,54 @@
+import wx
+
+GREY  = (180, 180, 180)
+BLACK = (0, 0, 0)
+
+class DownLoaderForm(wx.Panel):
+
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
+	downloadSizer = self.setup_download_sizer()
+	connectSizer = self.setup_connect_sizer()
+
+        topSizer = wx.BoxSizer(wx.VERTICAL)
+	topSizer.Add(connectSizer, 0, wx.TOP|wx.LEFT, 0)
+	topSizer.Add(downloadSizer, 0, wx.TOP|wx.LEFT, 0)
+        self.SetSizer(topSizer)
+
+    def setup_connect_sizer(self):
+	statBoxDownload = wx.StaticBox(self, wx.ID_ANY, '  Connect')
+	statBoxDownload.SetBackgroundColour(GREY)
+	statBoxDownload.SetForegroundColour(BLACK)
+        statBoxSizer = wx.StaticBoxSizer(statBoxDownload, wx.VERTICAL)
+
+        txtNoConnection = wx.StaticText(self, wx.ID_ANY, 'Not connected')
+        btnConnect = wx.Button(self, wx.ID_ANY, 'Connect')
+        txtNull = wx.StaticText(self, wx.ID_ANY, ' ')
+
+	connectSizer = wx.BoxSizer(wx.HORIZONTAL)
+        connectSizer.Add(btnConnect, 0, wx.BOTTOM|wx.LEFT, 20)
+        connectSizer.Add(txtNoConnection, 0, wx.TOP|wx.LEFT, 6)
+
+        statBoxSizer.Add(txtNull, 0, wx.LEFT, 1000)
+        statBoxSizer.Add(connectSizer, 0, wx.LEFT, 10)
+
+	return statBoxSizer
+
+    def setup_download_sizer(self):
+	statBoxDownload = wx.StaticBox(self, wx.ID_ANY, '  Downloader')
+	statBoxDownload.SetBackgroundColour(GREY)
+	statBoxDownload.SetForegroundColour(BLACK)
+        statBoxSizer = wx.StaticBoxSizer(statBoxDownload, wx.VERTICAL)
+
+        txtNull = wx.StaticText(self, wx.ID_ANY, ' ')
+
+        ascenderVersion = wx.StaticText(self, -1, "Ascender Version:")
+        remoteVersion = wx.StaticText(self, -1, "Remote Version:")
+
+        btnDownload = wx.Button(self, wx.ID_ANY, 'Download')
+        statBoxSizer.Add(btnDownload, 0, wx.ALL, 20)
+        statBoxSizer.Add(ascenderVersion, 0, wx.ALL, 20)
+        statBoxSizer.Add(txtNull, 0, wx.LEFT, 1000)
+        statBoxSizer.Add(remoteVersion, 0, wx.ALL, 20)
+
+	return statBoxSizer
--- prod_test_tool.py
+++ prod_test_tool.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 import wx
-#import wx.lib.scrolledpanel
 import time
 import wx.lib.scrolledpanel as scrolled
+import downloader
 
 WINDOW_SIZE = (1035, 870)
 
@@ -13,62 +13,11 @@ TEXT_SERIAL_PORT_BORDER = 10
 HEADLINE = '                                                             Production Test Tool, Ascender ACX/TCX'
 
 # color codes
-GREY    = (180, 180, 180)
-BLACK   = (0, 0, 0)
+GREY  = (180, 180, 180)
+BLACK = (0, 0, 0)
 
 class SerialSizer():
     pass
-
-
-class DownLoaderForm(wx.Panel):
-
-    def __init__(self, parent):
-        wx.Panel.__init__(self, parent)
-	downloadSizer = self.setup_download_sizer()
-	connectSizer = self.setup_connect_sizer()
-
-        topSizer = wx.BoxSizer(wx.VERTICAL)
-	topSizer.Add(connectSizer, 0, wx.TOP|wx.LEFT, 0)
-	topSizer.Add(downloadSizer, 0, wx.TOP|wx.LEFT, 0)
-        self.SetSizer(topSizer)
-
-    def setup_connect_sizer(self):
-	statBoxDownload = wx.StaticBox(self, wx.ID_ANY, '  Connect')
-	statBoxDownload.SetBackgroundColour(GREY)
-	statBoxDownload.SetForegroundColour(BLACK)
-        statBoxSizer = wx.StaticBoxSizer(statBoxDownload, wx.VERTICAL)
-
-        txtNoConnection = wx.StaticText(self, wx.ID_ANY, 'Not connected')
-        btnConnect = wx.Button(self, wx.ID_ANY, 'Connect')
-        txtNull = wx.StaticText(self, wx.ID_ANY, ' ')
-
-	connectSizer = wx.BoxSizer(wx.HORIZONTAL)
-        connectSizer.Add(btnConnect, 0, wx.BOTTOM|wx.LEFT, 20)
-        connectSizer.Add(txtNoConnection, 0, wx.TOP|wx.LEFT, 6)
-
-        statBoxSizer.Add(txtNull, 0, wx.LEFT, 1000)
-        statBoxSizer.Add(connectSizer, 0, wx.LEFT, 10)
-
-	return statBoxSizer
-
-    def setup_download_sizer(self):
-	statBoxDownload = wx.StaticBox(self, wx.ID_ANY, '  Downloader')
-	statBoxDownload.SetBackgroundColour(GREY)
-	statBoxDownload.SetForegroundColour(BLACK)
-        statBoxSizer = wx.StaticBoxSizer(statBoxDownload, wx.VERTICAL)
-
-        txtNull = wx.StaticText(self, wx.ID_ANY, ' ')
-
-        ascenderVersion = wx.StaticText(self, -1, "Ascender Version:")
-        remoteVersion = wx.StaticText(self, -1, "Remote Version:")
-
-        btnDownload = wx.Button(self, wx.ID_ANY, 'Download')
-        statBoxSizer.Add(btnDownload, 0, wx.ALL, 20)
-        statBoxSizer.Add(ascenderVersion, 0, wx.ALL, 20)
-        statBoxSizer.Add(txtNull, 0, wx.LEFT, 1000)
-        statBoxSizer.Add(remoteVersion, 0, wx.ALL, 20)
-
-	return statBoxSizer
 
 
 class Calib(wx.Panel):
@@ -512,7 +461,7 @@ class MainFrame(wx.Frame):
         nb = wx.Notebook(p)
  
         # Create the tab windows
-        self.tabDownLoader = DownLoaderForm(nb)
+        self.tabDownLoader = downloader.DownLoaderForm(nb)
         tabCalib = Calib(nb)
         self.tabProdTest = ProdTestForm(nb)
  
