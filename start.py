85657f44c4974481baf26fee76c8b038a1e5157f
heinz.samuelsson <heinz.samuelsson@unjo.com>
2017-06-20 09:28:48 +0200

    Added thread to start.py

--- start.py
+++ start.py
@@ -2,15 +2,34 @@ import os
 import time
 import sys
 import subprocess
+import threading
 
-os.system('./matplot.py&')
-time.sleep(2)
+class PollAlignment(threading.Thread):
 
-# get pid
-out = os.popen('pgrep -f matplot.py').readlines()
-out2 = out[0].rstrip('\n')
+    def __init__(self):
+        th = threading.Thread.__init__(self)
+	self.setDaemon(True)
+        self.start()    # start the thread
+ 
+    def run(self):
+        os.system('./matplot.py&')
 
-var = raw_input("Enter -> ")
 
-# kill matplotlib
-proc = subprocess.Popen(['kill -9 '+out2], stdout=subprocess.PIPE, shell=True)
+def main():
+
+    PollAlignment()
+    time.sleep(0.5)
+
+    # get pid
+    out = os.popen('pgrep -f matplot.py').readlines()
+    out2 = out[0].rstrip('\n')
+
+    
+    var = raw_input("Enter -> ")
+    
+    # kill matplotlib
+    proc = subprocess.Popen(['kill -9 '+out2], stdout=subprocess.PIPE, shell=True)
+
+
+if __name__ == '__main__':
+    main()
